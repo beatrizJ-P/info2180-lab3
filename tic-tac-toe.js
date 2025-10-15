9d17b07eab41e4200e31be1de3f799173abcf6f4
beatrizJ-P <beatrice.matthias@mymona.uwi.edu>
2025-10-14 22:06:27 -0500

    Attempted Exercise 2 of lab sheet, exercise incomplete

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -14,7 +14,30 @@ window.onload = function() {
 
     const square = board.querySelectorAll('div');
         
-        square.forEach(square => {
-            square.classList.add('square');
-        });
+    square.forEach(square => {
+        square.classList.add('square');
+    });
+
+    let play, lastPlay;
+    let playerOne = 'X';
+    let playerTwo = 'O';
+    board.addEventListener('click', function(e) {
+        if (e.target.classList.contains('square') && e.target.innerHTML === "") {
+            
+            if (lastPlay == playerOne) {
+                play = playerTwo;
+                e.target.innerHTML = play;
+                e.target.classList.add('O');
+                lastPlay = play;
+            } else{
+                play = playerOne;
+                e.target.innerHTML = play;
+                e.target.classList.add('X');
+                lastPlay = play;
+            }
+        };
+    });
+
+
 };
+
